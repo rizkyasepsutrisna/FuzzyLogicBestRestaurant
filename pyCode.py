c9812ec5e15195b7b53585e761ac3ba0ca53ff93
Rizky Asep Sutrisna <rizkyasepsutrisna@gmail.com>
2021-05-02 15:20:26 +0700

    Final Commit

--- pyCode.py
+++ pyCode.py
@@ -1,139 +1,155 @@
-import os
-import pandas as pd
 import csv
 
-pRendah = 0
-pStandar = 0
-pTinggi = 0
-mKecil = 0
-mSedang = 0
-mBesar = 0
-N1 = 0
-N2 = 0
+# Untuk memberikan nilai pada pelayanan suatu restoran
+nilaiRendah = 0
+nilaiStandar = 0
+nilaiTinggi = 0
+# ----------------------------------------------------
+
+# Untuk memberikan nilai pada makanan suatu restoran
+nilaiKecil = 0
+nilaiSedang = 0
+nilaiBesar = 0
+# ----------------------------------------------------
+
+# Untuk perhitungan dari hasil nilai
+nilaiSatu = 0
+nilaiDua = 0
+# ----------------------------------------------------
+
+# Untuk memberikan nilai kepada sebuah pelayanan dalam range 1-100
 
 
-def fuzzifikasiPelayanan(nPelayanann):
-    global pRendah, pStandar, pTinggi
+def fuzzifikasiPelayanan(nLayanan):
+    global nilaiRendah, nilaiStandar, nilaiTinggi
 
-    if (nPelayanann > 0 and nPelayanann <= 15):
-        pRendah = 1
-        pStandar = 0
-        pTinggi = 0
-    elif (nPelayanann > 15 and nPelayanann <= 30):
-        pRendah = (30 - nPelayanann) / (30 - 15)
-        pStandar = 0
-        pTinggi = 0
-    elif (nPelayanann > 30 and nPelayanann <= 45):
-        pRendah = 0
-        pStandar = (nPelayanann - 30) / (45 - 30)
-        pTinggi = 0
-    elif (nPelayanann > 45 and nPelayanann <= 60):
-        pRendah = 0
-        pStandar = 1
-        pTinggi = 0
-    elif (nPelayanann > 60 and nPelayanann <= 75):
-        pRendah = 0
-        pStandar = (75 - nPelayanann) / (75 - 60)
-        pTinggi = 0
-    elif (nPelayanann > 75 and nPelayanann <= 90):
-        pRendah = 0
-        pStandar = 0
-        pTinggi = (nPelayanann - 75) / (90 - 75)
-    elif (nPelayanann > 90 and nPelayanann <= 100):
-        pRendah = 0
-        pStandar = 0
-        pTinggi = 1
+    if (nLayanan > 0 and nLayanan <= 15):
+        nilaiRendah = 1
+        nilaiStandar = 0
+        nilaiTinggi = 0
+    elif (nLayanan > 15 and nLayanan <= 30):
+        nilaiRendah = (30 - nLayanan) / (30 - 15)
+        nilaiStandar = 0
+        nilaiTinggi = 0
+    elif (nLayanan > 30 and nLayanan <= 45):
+        nilaiRendah = 0
+        nilaiStandar = (nLayanan - 30) / (45 - 30)
+        nilaiTinggi = 0
+    elif (nLayanan > 45 and nLayanan <= 60):
+        nilaiRendah = 0
+        nilaiStandar = 1
+        nilaiTinggi = 0
+    elif (nLayanan > 60 and nLayanan <= 75):
+        nilaiRendah = 0
+        nilaiStandar = (75 - nLayanan) / (75 - 60)
+        nilaiTinggi = 0
+    elif (nLayanan > 75 and nLayanan <= 90):
+        nilaiRendah = 0
+        nilaiStandar = 0
+        nilaiTinggi = (nLayanan - 75) / (90 - 75)
+    elif (nLayanan > 90 and nLayanan <= 100):
+        nilaiRendah = 0
+        nilaiStandar = 0
+        nilaiTinggi = 1
 
-    return [pRendah, pStandar, pTinggi]
-
-
-def fuzzifikasiMakanan(nMakanann):
-    global mKecil, mSedang, mBesar
-
-    if (nMakanann <= 1.5):
-        mKecil = 1
-        mSedang = 0
-        mBesar = 0
-    elif (nMakanann <= 3):
-        mKecil = (3 - nMakanann) / (3 - 1.5)
-        mSedang = 0
-        mBesar = 0
-    elif (nMakanann <= 4.5):
-        mKecil = 0
-        mSedang = (nMakanann - 3) / (4.5 - 3)
-        mBesar = 0
-    elif nMakanann > 4.5 and nMakanann <= 6:
-        mKecil = 0
-        mSedang = 1
-        mBesar = 0
-    elif nMakanann > 6 and nMakanann <= 7.5:
-        mKecil = 0
-        mSedang = (7.5 - nMakanann) / (7.5 - 6)
-        mBesar = 0
-    elif nMakanann > 7.5 and nMakanann <= 9:
-        mKecil = 0
-        mSedang = 0
-        mBesar = (nMakanann - 7.5) / (9 - 7.5)
-    elif nMakanann > 9 and nMakanann <= 10:
-        mkecil = 0
-        mSedang = 0
-        mBesar = 1
-    return [mKecil, mSedang, mBesar]
+    return [nilaiRendah, nilaiStandar, nilaiTinggi]
 
 
-def ruleInferensi(pRendah, pStandar, pTinggi, mKecil, mSedang, mBesar):
-    global nilaiX, nilaiY
+def fuzzifikasiMakanan(nMakan):
+    global nilaiKecil, nilaiSedang, nilaiBesar
 
-    x1 = min(pTinggi, mBesar)
-    x2 = min(pTinggi, mSedang)
-    y1 = min(pTinggi, mKecil)
-    x3 = min(pStandar, mBesar)
-    x4 = min(pStandar, mSedang)
-    y2 = min(pStandar, mKecil)
-    x5 = min(pRendah, mBesar)
-    x6 = min(pRendah, mSedang)
-    y3 = min(pRendah, mKecil)
-    nilaiX = max(x1, x2, x3, x4, x5, x6)
-    nilaiY = max(y1, y2, y3)
+    if (nMakan <= 1.5):
+        nilaiKecil = 1
+        nilaiSedang = 0
+        nilaiBesar = 0
+    elif (nMakan <= 3):
+        nilaiKecil = (3 - nMakan) / (3 - 1.5)
+        nilaiSedang = 0
+        nilaiBesar = 0
+    elif (nMakan <= 4.5):
+        nilaiKecil = 0
+        nilaiSedang = (nMakan - 3) / (4.5 - 3)
+        nilaiBesar = 0
+    elif nMakan > 4.5 and nMakan <= 6:
+        nilaiKecil = 0
+        nilaiSedang = 1
+        nilaiBesar = 0
+    elif nMakan > 6 and nMakan <= 7.5:
+        nilaiKecil = 0
+        nilaiSedang = (7.5 - nMakan) / (7.5 - 6)
+        nilaiBesar = 0
+    elif nMakan > 7.5 and nMakan <= 9:
+        nilaiKecil = 0
+        nilaiSedang = 0
+        nilaiBesar = (nMakan - 7.5) / (9 - 7.5)
+    elif nMakan > 9 and nMakan <= 10:
+        nilaiKecil = 0
+        nilaiSedang = 0
+        nilaiBesar = 1
+    return [nilaiKecil, nilaiSedang, nilaiBesar]
 
-    return [nilaiX, nilaiY]
+
+def ruleInferensi(nilaiRendah, nilaiStandar, nilaiTinggi, nilaiKecil, nilaiSedang, nilaiBesar):
+    global hasilSatu, hasilDua
+
+    x1 = min(nilaiTinggi, nilaiBesar)
+    x2 = min(nilaiTinggi, nilaiSedang)
+    y1 = min(nilaiTinggi, nilaiKecil)
+    x3 = min(nilaiStandar, nilaiBesar)
+    x4 = min(nilaiStandar, nilaiSedang)
+    y2 = min(nilaiStandar, nilaiKecil)
+    x5 = min(nilaiRendah, nilaiBesar)
+    x6 = min(nilaiRendah, nilaiSedang)
+    y3 = min(nilaiRendah, nilaiKecil)
+    hasilSatu = max(x1, x2, x3, x4, x5, x6)
+    hasilDua = max(y1, y2, y3)
+
+    return [hasilSatu, hasilDua]
 
 
-def sugenoDefuzzyfikasi(N1, N2, i):
-    global listHasil
-    Nx = float((N1 * 50) + (N2 * 100))
-    Ny = float((N1 + N2))
-    if (Nx == 0 and Ny == 0):
+def SugenoDefuzzyfikasi(nilaiSatu, nilaiDua, batas):
+    global hasilNilai
+    hasilNilaiSatu = float((nilaiSatu * 50) + (nilaiDua * 100))
+    hasilNilaiDua = float((nilaiSatu + nilaiDua))
+    if (hasilNilaiSatu == 0 and hasilNilaiDua == 0):
         a = 0
     else:
-        a = Nx/Ny
+        a = hasilNilaiSatu/hasilNilaiDua
 
     if a >= 70 and a < 100:
-        listHasil.append(i)
+        hasilNilai.append(i)
     elif a == 100:
-        listHasil.append(i)
-    return listHasil
+        hasilNilai.append(i)
+    return hasilNilai
 
 
 def Output():
-    for i in range(0, len(listHasil)):
-        fuzzifikasiPelayanan(nPelayanann=listHasil[i][1])
-        fuzzifikasiMakanan(nMakanann=listHasil[i][2])
-        ruleInferensi(pRendah, pStandar, pTinggi, mKecil, mSedang, mBesar)
-        sugenoDefuzzyfikasi(N1, N2, i)
-    return sugenoDefuzzyfikasi(N1, N2, i)
+    for i in range(0, len(hasilNilai)):
+        fuzzifikasiPelayanan(nLayanan=hasilNilai[i][1])
+        fuzzifikasiMakanan(nMakan=hasilNilai[i][2])
+        ruleInferensi(nilaiRendah, nilaiStandar, nilaiTinggi, nilaiKecil, nilaiSedang, nilaiBesar)
+        SugenoDefuzzyfikasi(nilaiSatu, nilaiDua, i)
+    return SugenoDefuzzyfikasi(nilaiSatu, nilaiDua, i)
 
 
-pRendah = 0
-pStandar = 0
-pTinggi = 0
-mKecil = 0
-mSedang = 0
-mBesar = 0
-N1 = 0
-N2 = 0
+# Nilai untuk pelayanan
+nilaiRendah = 0
+nilaiStandar = 0
+nilaiTinggi = 0
+# ----------------------------------------------------
+
+# Nilai untuk makanan
+nilaiKecil = 0
+nilaiSedang = 0
+nilaiBesar = 0
+# ----------------------------------------------------
+
+# Nilai akhir
+nilaiSatu = 0
+nilaiDua = 0
 bantuan = []
 listBantuan = []
+# ----------------------------------------------------
 
 labelKolom = ['id', 'pelayanan', 'makanan']
 with open('restoran.csv') as rdFile:
@@ -160,15 +176,15 @@ with open('restoran.csv') as rdFile:
                     pelayanan.append(float(row[1]))
                     makanan.append(float(row[2]))
 
-listHasil = []
+hasilNilai = []
 for i in range(0, len(pelayanan)):
-    listHasil.append([x[i], pelayanan[i], makanan[i]])
+    hasilNilai.append([x[i], pelayanan[i], makanan[i]])
 
 
-with open('testing.csv', 'w') as newFile:
+with open('BestRestoran.csv', 'w') as newFile:
     tulisFile = csv.writer(newFile, delimiter=',')
-    for i in range(0, 10):
+    for i in range(0, 5):
         tulisFile.writerow([Output()[i][0]])
         tulisFile.writerow([Output()[i][1]])
 
-print("Testing.csv is ready!")
+print("BestRestoran.csv is ready!")
